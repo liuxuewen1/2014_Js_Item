d18906da0aea1b488d0936be5ae4038e1bc1b19e
liuxuewen1 <457460089@qq.com>
2014-05-24 16:24:55 +0800

    兼容的常用的公用方法

---
+++ common.js
@@ -0,0 +1,16 @@
+
+//得到元素某个属性值（带px）  
+function getAttr(obj,attr){
+	return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj,false)[attr];		
+}
+	
+//得到obj的定位距离
+function getPos(obj){
+	var l=0,t=0;
+	while(obj){
+		l+=obj.offsetLeft;
+		t+=obj.offsetTop;
+		obj=obj.offsetParent;
+	}
+	return {left:l, top:t};
+}
