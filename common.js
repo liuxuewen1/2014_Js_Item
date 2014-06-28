7cf0367dbe8686027f41c250b9063cfb092dec1f
liuxuewen1 <457460089@qq.com>
2014-06-28 18:20:07 +0800

    修改原生态ajax的bug

--- common.js
+++ common.js
@@ -113,62 +113,78 @@ functioin delCookie(name){
 
 //2014-6-20 原生态Ajax写法
 function ajax(obj){
+	//参数默认值设置
 	obj=obj || {};
 	if(!obj.url){
-		alert('URL为必填参数，请填写');
+		alert('URL必填，请求失败');
 		return;
 	}
-	obj.type=obj.type || 'Get';
+	obj.type=obj.type || 'GET';
 	obj.data=obj.data || {};
 	obj.timeout=obj.timeout || 3;
-	
-	//建立Ajax对象
+
+	//1、建立oAjax对象
+	//XMLHttpRequest兼容IE7、IE7+、FF、Chrome等各种浏览器
 	if(window.XMLHttpRequest){
 		var oAjax=new XMLHttpRequest();
 	}else{
-		var oAjax=new ActionXObject("Microsoft.XMLHTTP");
+		//兼容IE5、IE6
+		var oAjax=new ActiveXObject('Microsoft.XMLHTTP');
 	}
-	//打开连接、发送消息
+	
+	//2、打开连接、发送请求
 	switch(obj.type.toLowerCase()){
-		case "get":
-			oAjax.open("GET",obj.url+'?'+jsonToURL(obj.data),true);
+		case 'get':
+			oAjax.open('GET',obj.url+'?'+jsonToURL(obj.data),true);
 			oAjax.send();
 			break;
-		case "post":
-			oAjax.open("GET",obj.url,true);
-			oAjax.setRequestHeader("content-type","application/x-www-form-urlencoded");
+		case 'post':
+			oAjax.open('POST',obj.url,true);
+			//post时需要设置请求头
+			oAjax.setRequestHeader('content-type','application/x-www-form-urlencoded');
 			oAjax.send(jsonToURL(obj.data));
 			break;
 	}
 	
-	obj.loadingFn && obj.loadingFn();	//请求完成后执行
+	//如果有正在加载函数 执行
+	obj.loadingFn && obj.loadingFn();
 	
-	//通讯信息
+	//3、监听事件：是否成功返回回来数据
 	oAjax.onreadystatechange=function(){
-		//0：准备 1：Ajax对象准备完毕 2：接收完成，数据-编码-加密 3：解析数据 4：完成
+		//readyState：0-准备状态；1-ajax准备完毕；2-接收完毕；3-正在解析；4-数据接受完成
 		if(oAjax.readyState!=4) return;
 		
-		obj.completeFn && obj.completeFn();//接收完成时执行
+		//请求完成，如果有完成函数 执行
+		obj.completeFn && obj.completeFn();
 		
-		//200~300：成功 304：未修改
+		//status：2开头表示成功；304表示文件未修改-成功
 		if(oAjax.status>=200 && oAjax.status<300 || oAjax.status==304){
+			//成功，清除超时定时器
 			clearTimeout(timer);
+			
+			//responseText：请求到的数据（常用）
+			//responseXML：请求到的xml数据（很少用），然后在succFn中跟使用DOM一样解析XML使用，稍麻烦
 			obj.succFn && obj.succFn(oAjax.responseText);
 		}else{
+			//失败，清除超时定时器
 			clearTimeout(timer);
+			
 			obj.failFn && obj.failFn(oAjax.status);
 		}
+		
 	}
 	
-	//超时
+	//设置超时时间
 	var timer=setTimeout(function(){
-		alert("网络超时");
 		oAjax.onreadystatechange=null;
+		alert('请求超时');
 	},obj.timeout*1000);
 }
+
 function jsonToURL(json){
-	var arr=[];
+	json=json || {};
 	json.t=Math.random();
+	var arr=[];
 	for(var key in json){
 		arr.push(key+'='+json[key]);
 	}
